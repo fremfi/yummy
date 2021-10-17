1b218956ca9b355677a9d1def20fcaf29cc83a76
Frederick Mfinanga <frederick@outschool.com>
2021-10-16 23:03:35 -0400

    feat: add firebase

--- lib/infrastructure/recipe/recipe_repository.dart
+++ lib/infrastructure/recipe/recipe_repository.dart
@@ -1,10 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:yummy/domain/recipe/recipe.dart';
 import 'package:yummy/domain/recipe/recipe_repository.dart';
 
 class RecipeRepositoryImpl implements RecipeRepository {
+  final CollectionReference _recipesCollectionReference =
+      FirebaseFirestore.instance.collection('recipes');
+
   @override
-  Future<List<Recipe>> getRecipes() {
-    // TODO: Get recipes from the database
-    return Future<List<Recipe>>.value([]);
+  Future<List<Recipe>> getRecipes() async {
+    List<Recipe> recipes = [];
+    QuerySnapshot querySnapshot = await _recipesCollectionReference.get();
+
+    for (var doc in querySnapshot.docs) {
+      recipes.add(Recipe.fromJson(doc.data()! as Map<String, dynamic>));
+    }
+    return recipes;
+    // return Future<List<Recipe>>.value([]);
   }
 }
